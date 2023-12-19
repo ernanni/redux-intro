a351d606c20cd80a169aa8aa746dc4e8e9dc5a23
Ernanni <ernannicf@gmail.com>
2023-12-19 01:25:10 -0300

    adding redux thunk and a middleware

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -19,8 +19,9 @@ function AccountOperations() {
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount('');
+    setCurrency('USD');
   }
 
   function handleWithdrawal() {
@@ -62,7 +63,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={account.isLoading}>
+            {account.isLoading ? 'Converting...' : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -2,12 +2,17 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
@@ -27,13 +32,31 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: '',
         balance: state.balance - state.loan,
       };
+    case 'account/convertingCurrency':
+      return {
+        ...state,
+        isLoading: true,
+      };
     default:
       return state;
   }
 }
 
-function deposit(amount) {
-  return { type: 'account/deposit', payload: amount };
+function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  //Redux Thunk is used when we make an external request in order to proceed with state change asynchronously
+  return async (dispatch, getState) => {
+    dispatch({ type: 'account/convertingCurrency' });
+    //API call -- middleware
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
 }
 
 function withdraw(amount) {

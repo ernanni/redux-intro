69c86fa0af4a33f0e31593db5ebfc037ba172753
Ernanni <ernannicf@gmail.com>
2023-12-19 05:19:25 -0300

    convert redux into redux toolkit

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,11 +1,75 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            purpose,
+          },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        const { amount, purpose } = action.payload;
+        state.loan = amount;
+        state.loanPurpose = purpose;
+        state.balance += amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loanPurpose = '';
+      state.loan = 0;
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+// added thunk using the oldschool redux
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  //Redux Thunk is used when we make an external request in order to proceed with state change asynchronously
+  return async (dispatch, getState) => {
+    dispatch({ type: 'account/convertingCurrency' });
+    //API call -- middleware
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
+}
+
+export default accountSlice.reducer;
+
+/* -- OLDSCHOOL REDUX --
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case 'account/deposit':
       return {
@@ -75,3 +139,4 @@ function payLoan() {
 }
 
 export { deposit, withdraw, requestLoan, payLoan };
+*/
--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,10 +1,44 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   fullName: '',
   nationalID: '',
   createdAt: '',
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID, createdAt) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        const { fullName, nationalID, createdAt } = action.payload;
+        state.fullName = fullName;
+        state.nationalID = nationalID;
+        state.createdAt = createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/* -- OLDSCHOOL REDUX --
+export default function customerReducer(state = initialState, action) {
   switch (action.type) {
     case 'customer/createCustomer':
       const { fullName, nationalID, createdAt } = action.payload;
@@ -32,3 +66,4 @@ function updateName(fullName) {
 }
 
 export { createCustomer, updateName };
+*/
